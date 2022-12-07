c8cb548303ba532d3b9fb11da0647f458988edc8
vikyw89 <vikyw89@gmail.com>
2022-12-07 17:59:01 +0800

    checkpoint

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title,author,pages) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary({title, author, pages}) {
+    console.log(addBookToLibrary)
+    myLibrary.push(Object.create(Book(title, author, pages)))
+    console.log(myLibrary)
 }
